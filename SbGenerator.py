a1a45eea2e6116c5d645bdb955a68ff4121d0ff2
MihailRis <mexaruc@gmail.com>
2016-08-25 15:35:12 +0400

    SbGenerator v.2.8

--- SbGenerator.py
+++ SbGenerator.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
-#WORLD GENERATION
+#Zendes 2.5 WORLD GENERATION 2.8
 
-# seeds 707
+#Генератор мира для игры Zendes 2.5
+
+
+#import noise # Шум Перлина (пока не используется в этом генераторе)
 import random
-from SbDev import sb_output
-from SbDev import divis_get
-import math
 
-bs = 45
-wdm = 45*bs*0
-wum = 30*bs*0
+bs = 45 # размер блока
+bsInv = 1.0/float(bs)
 
 gs = 15*bs #GenerationSise
-ogs = int(gs/bs) #GenerationSise in blocks
+ogs = int(gs*bsInv) #GenerationSise in blocks
 
-PlainsB = (65*bs+wdm, 60*bs+wum, 3, 50, 100) # world_down, world_up, up_blocks, Trees, chance
-DesertB = (55*bs+wdm, 50*bs+wum, 5, 0, 60)
-MountainsB = (40*bs+wdm, 30*bs+wum, 1, 90, 200)
-ForestB = (80*bs+wdm, 60*bs+wum, 3, 4, 100)
-SeaB = (60*bs+wdm, 60*bs+wum, 6, 0, 500)
+#Biom configs
+PlainsB = (235*bs, 230*bs, 3, 50, 100) # world_down, world_up, up_blocks, Trees, chance
+DesertB = (225*bs, 220*bs, 5, 0, 60)
+MountainsB = (210*bs, 200*bs, 1, 90, 70)
+ForestB = (250*bs, 230*bs, 3, 4, 100)
+SeaB = (268*bs, 264*bs, 5, 0, 50)
 
-BIOMS0 = [PlainsB, DesertB, MountainsB, ForestB]
+BIOMS0 = [PlainsB, DesertB, MountainsB, ForestB, SeaB]
 BIOMS = []
+
+# Генерация списка с биомами с колличеством их копий которое указано в chance в кортеже биома
 for BIOM in BIOMS0:
 	maked = 0
 	to_make = BIOM[4]
@@ -31,170 +33,101 @@ for BIOM in BIOMS0:
 
 
 
+# Главная функция - генератор мира
 def generation(seed, mode, x, y, flatdata=10):
-	if mode == "flat":
-		if y/bs - 6 >= flatdata:
-			a = random.randint(0,1000)
-			z = False
-			if a < 35 and a > 20:
-				return 4
-				z = True
-			if not z and a < 20 and a > 10 and y/bs > flatdata+10:
-				return 9
-				z = True
-			if not z:
-				return 1
-		if y/bs - 2 >= flatdata:
-			return 2
-		if y/bs - 1 >= flatdata:
-			return 3
-		if y/bs - 4 < flatdata:
-			return 0
-	if mode == "empty":
-		return 0
+	# seed - interger или float ключ генерации (любое число) может быть отрицательным
+	# mode - string режим генерации (в файле сечас только "optim#normal")
+	# x, y - int кoординаты блока
+	# flatdata - interger больше не используется (высота в режиме плоского мира)
 
-	if mode == "interstellar":
-		r = seed*x*y
-		if divis_get(r, 2):
-			return(0)
-		else:
-			return(1)
 
-	if mode == "normal":
+	# основной режим генерации
+	if mode == "optim#normal":
+		lh_DIVggs = 1.0/float(gs)
+		point_hight = None
+		
 
-		left_point = int(x/gs)*gs
+		# определение левой и правой точек и их биомов
+		left_point = int(x*lh_DIVggs)*gs
 		random.seed(seed)
-		random.seed(int((left_point*100.0)/bs/gs/random.randint(10, 30)))
+		random.seed(int((left_point*100.0)*bsInv*lh_DIVggs/random.randint(4, 30)))
 		left_biom = BIOMS[random.randint(0, len(BIOMS)-1)]
 
-		right_point = (int(x/gs)*gs)+gs
+		right_point = (int(x*lh_DIVggs)*gs)+gs
 		random.seed(seed)
-		random.seed(int((right_point*100.0)/bs/gs/random.randint(10, 30)))
+		random.seed(int((right_point*100.0)*bsInv*lh_DIVggs/random.randint(4, 30)))
 		right_biom = BIOMS[random.randint(0, len(BIOMS)-1)]
+
+		# Если координаты нулевые то по-умолчанию ставится биом Plains - равнина
+		# (биом под персонажем в начале)
+		if int(left_point*bsInv*bsInv) == 0:
+			left_biom = PlainsB
+
+		if int(right_point*bsInv*bsInv) == 0:
+			right_biom = PlainsB
 
 		#определение высот 2-х основных точек, левой и правой
 		random.seed(seed*left_point*gs)
-		left_point_H = int(random.randint(left_biom[1], left_biom[0])/bs)*bs
+		left_point_H = int(random.randint(left_biom[1], left_biom[0])*bsInv)*bs
 		random.seed(seed*right_point*gs)
-		right_point_H = int(random.randint(right_biom[1], right_biom[0])/bs)*bs
+		right_point_H = int(random.randint(right_biom[1], right_biom[0])*bsInv)*bs
 		biom = PlainsB
+
 		#если блок совподает с левой точкой
-		if int(left_point/bs)*bs == int(x/bs)*bs:
+		if int(left_point*bsInv)*bs == int(x*bsInv)*bs:
 			point_hight = left_point_H
 			biom = left_biom
+
 		#если блок совподает с правой точкой
-		if int(right_point/bs)*bs == int(x/bs)*bs:
+		if int(right_point*bsInv)*bs == int(x*bsInv)*bs:
 			point_hight = right_point_H
 			biom = right_biom
-		left_percent = float(float(x-left_point)/gs)
-		right_percent = float(float(right_point-x)/gs)
-		try:
-			point_hight
-		except NameError:
-			rsn = 0
-			point_hight = (left_point_H*right_percent)+(right_point_H*left_percent)
 
+
+		if point_hight is None: #Если высота блока ещё не определена:
+			rsn = 0
+
+			#Определения влияния точек на блок
+			left_percent = float(x-left_point)*lh_DIVggs
+			right_percent = float(right_point-x)*lh_DIVggs
+
+			# Влияние высоты точек на высоту блока
+			lpecented = left_point_H*right_percent
+			rpercented = right_point_H*left_percent
+
+			#Определение высоты блока
+			point_hight = lpecented+rpercented
+
+			#Определение биома для блока
 			random.seed(seed+x+y+point_hight)
-			if random.randint(-(int(left_point_H*left_percent*100)), int(right_point_H*right_percent*100)) >= 0:
+			if random.randint(-(int(lpecented*100)), int(rpercented*100)) >= 0:
 				biom = left_biom
 			else:
 				biom = right_biom
 
+		# Выбор id блока
 		if y > point_hight:
-			if int(y/bs) == int(point_hight/bs):
+			if int(y*bsInv) == int(point_hight*bsInv):
 				return 6
 			if y - 6*bs <= point_hight:
 				return biom[2]
 			else:
+				#Генерация руд (random может быть заменён на шум Перлина для оптимизации)
 				random.seed(seed+x+y+seed-point_hight+biom[0]-biom[1]*x*y*x**2)
 				rand = random.randint(0,20000)
 				to_id = 1
+				# Угольная руда
 				if rand < 250:
 					to_id = 4
+				# Железная руда
 				if rand < 40 and point_hight+(20*bs) < y:
 					to_id = 9
+				# Алмазы
 				if rand < 5 and point_hight+(100*bs) < y:
 					to_id = 6
 				return to_id
 		else:
-			random.seed(seed+x)
-			if biom[3] != 0:
-				done_1 = False
-				if random.randint(0, biom[3]) == 0:
-					if int(y/bs) == int(point_hight/bs):
-						return 7
-						done_1 = True
-					if int(y/bs)+1 == int(point_hight/bs):
-						return 7
-						done_1 = True
-					if int(y/bs)+2 == int(point_hight/bs):
-						return 7
-						done_1 = True
-					if int(y/bs)+3 == int(point_hight/bs):
-						return 7
-						done_1 = True
-					if int(y/bs)+4 == int(point_hight/bs):
-						return 7
-						done_1 = True
-					if int(y/bs)+5 == int(point_hight/bs):
-						return 8
-					if int(y/bs)+6 == int(point_hight/bs):
-						return 8
-						done_1 = True
-
-				random.seed(seed+x+bs)
-				if not done_1 and random.randint(0, biom[3]) == 0:
-					if int(y/bs)+1 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+2 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+3 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+4 == int(point_hight/bs):
-						return 8
-						done_1 = True
-				random.seed(seed+x-bs)
-				if not done_1 and random.randint(0, biom[3]) == 0:
-					if int(y/bs)+1 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+2 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+3 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+4 == int(point_hight/bs):
-						return 8
-						done_1 = True
-				random.seed(seed+x+bs*2)
-				if not done_1 and random.randint(0, biom[3]) == 0:
-					if int(y/bs)+2 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+3 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+4 == int(point_hight/bs):
-						return 8
-						done_1 = True
-				random.seed(seed+x-bs*2)
-				if not done_1 and random.randint(0, biom[3]) == 0:
-					if int(y/bs)+2 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+3 == int(point_hight/bs):
-						return 8
-						done_1 = True
-					if int(y/bs)+4 == int(point_hight/bs):
-						return 8
-						done_1 = True
-
-				if not done_1:
-					return 0
-			else:
-				return 0
-#		x_left_rasn = float(ogs/(x-left_point+0.1))
+			# уровень моря
+			if int(y*bsInv) >= 251:
+				return 11
+		return 0
