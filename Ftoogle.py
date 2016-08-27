9924b5f4521a4f516de9682af0af4546ab00ff98
Daniil Yarancev <TiberiumPY@users.noreply.github.com>
2016-08-27 13:03:35 +0300

    make ftoogle.py simpler

--- Ftoogle.py
+++ Ftoogle.py
@@ -1,23 +1,11 @@
 ''' the tooogle function '''
-''' toogle function get oen argument: boolean or interger object, True to False, 0 to 1, 1 to 0, False to True '''
+''' toogle function get one argument: boolean or interger object, True to False, 0 to 1, 1 to 0, False to True '''
 
-__autor__ = "MihqailRis"
+__author__ = "MihqailRis"
 __version__ = 1.0
 
 def toogle(value):
-    toogled = False
-    if type(value) == bool:
-        toogled = True
-        if value:
-            return False
-        else:
-            return True
-    if type(value) == int:
-        toogled = True
-        if value == 1:
-            return 0
-        elif value == 0:
-            return 1
-    if not toogled:
-        del toogled
-        raise TypeError, "not boolean object not toogled"
+    if type(value) is bool or type(value) is int:
+        return int(not value)
+    else:
+        raise TypeError("Can't toggle {0} object".format(value))
