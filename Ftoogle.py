3641c35e8f54ea19e116935d657b2f8330a43133
MihailRis <mexaruc@gmail.com>
2016-08-27 14:56:28 +0400

    Update Ftoogle.py

--- Ftoogle.py
+++ Ftoogle.py
@@ -1,7 +1,7 @@
 ''' the tooogle function '''
 ''' toogle function get one argument: boolean or interger object, True to False, 0 to 1, 1 to 0, False to True '''
 
-__author__ = "MihqailRis"
+__author__ = "MihailRis"
 __version__ = 1.0
 
 def toogle(value):
